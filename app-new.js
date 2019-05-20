64959908a9d08112382e470938e344d7aaeba339
Shuchi Joshi <shuchisd@gmail.com>
2019-05-20 00:04:26 -0400

    Adding array.filter function

--- app-new.js
+++ app-new.js
@@ -1,29 +1,34 @@
 function onReady() {
 
-  const toDos = [];
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  let idValue=0;
+
 
   addToDoForm.addEventListener('submit', event => {
       event.preventDefault();
       createNewToDo();
     });
 
-  renderTheUI();
-
   function createNewToDo() {
 
     const newToDoText = document.getElementById('newToDoText');
     if (!newToDoText.value) { return; }  // exit out if input text is empty
 
+
     toDos.push({
         title: newToDoText.value,
-        complete: false
+        complete: false,
+        id: idValue
       });
 
     // Clear initial text input
     newToDoText.value = '';
 
     renderTheUI();
+    idValue = idValue + 1;  // incrementing the ID for every new text input object created
+
+    //console.log(toDos['idValue'].id);
 
   }
 
@@ -36,12 +41,34 @@ function onReady() {
 
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const deleteButton = document.createElement('input');
+
+      deleteButton.type="button";
+      deleteButton.value="Delete";
       checkbox.type = "checkbox";
 
       newLi.textContent = toDo.title;
+      newLi.id = toDo.id;
+
+
+      // Dynamically add function for onclick event
+      deleteButton.onclick = function(){
+          var node = this.parentNode;
+          console.log(node.id);
+          toDos = toDos.filter(e => e.id !== node.id);
+          console.log(toDos);
+
+          renderTheUI();
+
+
+      //     //alert("Deleting "+ title); This will echo the item text getting deleted
+      //   //  node.parentNode.removeChild(node);  // Get the list item of the button, pass it to list container, call removeChild
+     };
+
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
 
     });
 
